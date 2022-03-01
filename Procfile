cb97127a86773418b6e4c295c76b11732b809def
Shahil.pu.inx <shahil.pu.inexture@gmail.com>
2022-03-01 11:53:37 +0530

    make it better

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn djninja.wsgi --log-file
---
+++ tracks/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+from django.db import models
+
+
+class Track(models.Model):
+    title = models.CharField(max_length=250)
+    artist = models.CharField(max_length=250)
+    duration = models.FloatField()
+    last_play = models.DateTimeField()
